7f1969a932b4c56252005eebab8916848c7f5bda
VELLANKI GAGAN <2400030198@kluniversity.in>
2026-05-02 19:05:26 +0530

    commit

---
+++ src/main/java/com/klef/fsad/exam/Transport.java
@@ -0,0 +1,44 @@
+package com.klef.fsad.exam;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "transport")
+public class Transport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int id;
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "transport_date")
+    public String date;
+
+    @Column(name = "status")
+    public String status;
+
+    @Column(name = "vehicle_type")
+    public String vehicleType;
+
+    @Column(name = "fare")
+    public double fare;
+
+    public Transport() {
+    }
+
+    public Transport(String name, String date, String status, String vehicleType, double fare) {
+        this.name = name;
+        this.date = date;
+        this.status = status;
+        this.vehicleType = vehicleType;
+        this.fare = fare;
+    }
+}
